713190995cc79ac87ce7196240b599c9351b66b6
abd03mg <eemgyg@gmail.com>
2024-04-05 04:08:52 +0200

    archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,15 @@ from os import makedirs
 def do_pack():
     """ Function that archive """
 
-    ArchName = "versions/web_static_{}".format(
-            dt.now().strftime("%Y%m%d%H%M%S"))
-    makedirs('versions', exist_ok=True)
+    try:
+        ArchName = "versions/web_static_{}".format(
+                dt.now().strftime("%Y%m%d%H%M%S"))
+        makedirs('versions', exist_ok=True)
 
-    ret = local("tar -cvzf {} web_static".format(ArchName))
-    if ret.succeeded:
-        return ret.command.split(" ")[2]
+        ret = local("tar -cvzf {} web_static".format(ArchName))
+        if ret.succeeded:
+            return ret.command.split(" ")[2]
+        else:
+            return None;
+    except Exception:
+        return None;
