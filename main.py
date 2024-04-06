444e3a65c6ebf76ccd1f8f856155bed12c5e8799
abd03mg <eemgyg@gmail.com>
2024-04-06 07:50:45 +0200

    d

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Fabric script that generates archive."""
+
+from fabric.api import *
+from os.path import exists
+
+
+env.hosts = ['18.209.179.152', '3.86.13.133']
+
+
+def do_deploy(archive_path):
+    """function that distributes
+    an archive to your web servers"""
+    try:
+        ArName = archive_path.split("/")[-1][0:-4]
+        dest = "/data/web_static/releases/"
+        if not (exists(archive_path)):
+            return False
+        put(archive_path, "/tmp/")
+        sudo("mkdir -p {}{}/".format(dest, ArName))
+        sudo("chown -R ubuntu:ubuntu /data/")
+        run("tar -xzf /tmp/{}.tgz -C {}{}/".format(ArName, dest, ArName))
+        run("rsync -a {}{}/web_static/* {}{}".format(dest, ArName,
+            dest, ArName))
+        run("rm -rf /tmp/{}.tgz".format(ArName))
+        run("rm -rf {}{}/web_static".format(dest, ArName))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {}{}/ /data/web_static/current".format(dest, ArName))
+    except Exception:
+        return False
+    return True
