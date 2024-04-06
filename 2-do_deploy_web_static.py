218ad3cbbaec9956dd1761c67da6c576addad025
abd03mg <eemgyg@gmail.com>
2024-04-06 06:56:56 +0200

    das

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,14 +5,12 @@ from fabric.api import *
 from os.path import exists
 
 
-env.user = "ubuntu"
 env.hosts = ['18.209.179.152', '3.86.13.133']
 
 
 def do_deploy(archive_path):
     """function that distributes
     an archive to your web servers"""
-    return True
     try:
         ArName = archive_path.split("/")[-1][0:-4]
         dest = "/data/web_static/releases/"
@@ -24,9 +22,10 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{}.tgz -C {}{}/".format(ArName, dest, ArName))
         run("rsync -a {}{}/web_static/* {}{}".format(dest, ArName,
             dest, ArName))
+        run("rm -rf /tmp/{}.tgz".format(ArName))
         run("rm -rf {}{}/web_static".format(dest, ArName))
         run("rm -rf /data/web_static/current")
         run("ln -sf {}{}/ /data/web_static/current".format(dest, ArName))
-        return True
     except Exception:
         return False
+    return True
