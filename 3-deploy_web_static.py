ea2b0bb41c895d3c14cb32c83f7c0b3c25b945d6
abd03mg <eemgyg@gmail.com>
2024-04-06 04:50:05 +0200

    3*

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,49 @@
 #!/usr/bin/python3
 """ Full deployment"""
-do_pack = __import__("1-pack_web_static").do_pack
-do_deploy = __import__("2-do_deploy_web_static").do_deploy
+
+from fabric.api import *
+from datetime import datetime as dt
+from os import makedirs
+from os.path import exists
+
+
+def do_pack():
+    """ Function that archive """
+
+    try:
+        ArchName = "versions/web_static_{}.tgz".format(
+                dt.now().strftime("%Y%m%d%H%M%S"))
+        makedirs('versions', exist_ok=True)
+
+        ret = local("tar -cvzf {} web_static".format(ArchName))
+        if ret.succeeded:
+            return ret.command.split(" ")[2]
+        else:
+            return None
+    except Exception:
+        return None
+
+
+def do_deploy(arch):
+    """function that distributes
+    an archive to your web servers"""
+    try:
+        ArName = arch.split("/")[-1][0:-4]
+        dest = "/data/web_static/releases/"
+        if not (exists(arch)):
+            return False
+        put(arch, "/tmp/")
+        run("mkdir -p {}{}/ ;".format(dest, ArName))
+        sudo("chown -R $(id -un):$(id -gn) /data/")
+        run("tar -xzf /tmp/{}.tgz -C {}{}".format(ArName, dest, ArName))
+        run("rsync -a {}{}/web_static/* {}{}/".format(dest, ArName,
+            dest, ArName))
+        run("rm -rf {}{}/web_static;".format(dest, ArName))
+        run("rm -rf /data/web_static/current;")
+        run("ln -sf {}{}/ /data/web_static/current".format(dest, ArName))
+        return True
+    except Exception:
+        return False
 
 
 def deploy():
