8605ec2c7b71b75c3f0c5f3855d6157c43f1de8e
abd03mg <eemgyg@gmail.com>
2024-04-06 02:22:00 +0200

    fds

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,30 +2,11 @@
 """ Fabric script that generates archive."""
 
 from fabric.api import *
-from datetime import datetime as dt
-from os import makedirs
 from os.path import exists
 
 
 env.user = "ubuntu"
 env.hosts = ['18.209.179.152', '3.86.13.133']
-
-
-def do_pack():
-    """ Function that archive """
-
-    try:
-        ArchName = "versions/web_static_{}.tgz".format(
-                dt.now().strftime("%Y%m%d%H%M%S"))
-        makedirs('versions', exist_ok=True)
-
-        ret = local("tar -cvzf {} web_static".format(ArchName))
-        if ret.succeeded:
-            return ret.command.split(" ")[2]
-        else:
-            return None
-    except Exception:
-        return None
 
 
 def do_deploy(arch):
@@ -37,11 +18,12 @@ def do_deploy(arch):
         if not (exists(arch)):
             return False
         put(arch, "/tmp/")
-        run("mkdir -p {}{}/ ;".format(dest, ArName))
+        sudo("mkdir -p {}{}/ ;".format(dest, ArName))
+        sudo("chown -R ubuntu:ubuntu /data/web_static")
         run("tar -xzf /tmp/{}.tgz -C {}{}/".format(ArName, dest, ArName))
-        run("mv {}{}/web_static/* {}{}/".format(dest, ArName, dest, ArName))
+        run("mv -f {}{}/web_static/* {}{}/".format(dest, ArName, dest, ArName))
         run("rm -rf {}{}/web_static;".format(dest, ArName))
         run("rm -rf /data/web_static/current;")
-        run("ln -s {}{}/ /data/web_static/current".format(dest, Arname))
+        run("ln -s {}{}/ /data/web_static/current".format(dest, ArName))
     except Exception:
         return False
